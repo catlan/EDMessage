bcc1e3679919ab74e735ff8706b1f3cd6e317d61
Christopher Atlan <catlan@restoroot.com>
2012-03-29 19:30:06 +0200

    Merge with EDCommons

---
+++ EDCommon.h
@@ -0,0 +1,36 @@
+//---------------------------------------------------------------------------------------
+//  EDCommon.h created by erik on Sat 05-Sep-1998
+//  @(#)$Id: EDCommon.h,v 2.9 2007-08-12 18:47:20 erik Exp $
+//
+//  Copyright (c) 1998-2003,2008 by Erik Doernenburg. All rights reserved.
+//
+//  Permission to use, copy, modify and distribute this software and its documentation
+//  is hereby granted, provided that both the copyright notice and this permission
+//  notice appear in all copies of the software, derivative works or modified versions,
+//  and any portions thereof, and that both notices appear in supporting documentation,
+//  and that credit is given to Erik Doernenburg in all documents and publicity
+//  pertaining to direct or indirect use of this code or its derivatives.
+//
+//  THIS IS EXPERIMENTAL SOFTWARE AND IT IS KNOWN TO HAVE BUGS, SOME OF WHICH MAY HAVE
+//  SERIOUS CONSEQUENCES. THE COPYRIGHT HOLDER ALLOWS FREE USE OF THIS SOFTWARE IN ITS
+//  "AS IS" CONDITION. THE COPYRIGHT HOLDER DISCLAIMS ANY LIABILITY OF ANY KIND FOR ANY
+//  DAMAGES WHATSOEVER RESULTING DIRECTLY OR INDIRECTLY FROM THE USE OF THIS SOFTWARE
+//  OR OF ANY DERIVATIVE WORK.
+//---------------------------------------------------------------------------------------
+
+#import <Foundation/Foundation.h>
+
+#import "EDCommonDefines.h"
+#import "EDObjcRuntime.h"
+
+#import "NSArray+Extensions.h"
+#import "NSAttributedString+Extensions.h"
+#import "NSData+Extensions.h"
+#import "NSObject+Extensions.h"
+#import "NSScanner+Extensions.h"
+#import "NSString+Extensions.h"
+
+#import "EDLightWeightLock.h"
+#import "EDMutableObjectPair.h"
+#import "EDObjectPair.h"
+#import "EDStringScanner.h"
--- Message.subproj/EDMessagePart.m
+++ Message.subproj/EDMessagePart.m
@@ -19,7 +19,7 @@
 //---------------------------------------------------------------------------------------
 
 #import <Foundation/Foundation.h>
-#import <EDCommon/EDCommon.h>
+#import "EDCommon.h"
 #import "utilities.h"
 #import "NSString+MessageUtils.h"
 #import "NSData+MIME.h"
